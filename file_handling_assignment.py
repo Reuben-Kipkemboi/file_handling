9da536bbc5543a5d74f15176c07da8429ec116fc
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2024-04-06 21:23:27 +0300

    ft:append - appends additional data to txt file

--- file_handling_assignment.py
+++ file_handling_assignment.py
@@ -1,11 +1,11 @@
 # Open the file "my_file.txt" in write mode ('w')
-# with open("my_file.txt", "w") as file:
-#     # Write three lines of text to the file
-#     file.write("Hello, Lets explore file handling by doing 1, 2, 3.\n")
-#     file.write("what if we do a multiplication of some numbers here : 4*4\n")
-#     file.write("Its already night, Can we do this tomorrow .\n")
+with open("my_file.txt", "w") as file:
+    # Write three lines of text to the file
+    file.write("Hello, Lets explore file handling by doing 1, 2, 3.\n")
+    file.write("what if we do a multiplication of some numbers here : 4*4\n")
+    file.write("Its already night, Can we do this tomorrow .\n")
 
-# print("We wrote into  'my_file.txt' and then we will read it later.")
+print("We wrote into  'my_file.txt' and then we will read it later.")
 
 # Open the file "my_file.txt" in read mode ('r')
 with open("my_file.txt", "r") as file:
@@ -15,3 +15,13 @@ with open("my_file.txt", "r") as file:
 # Display the contents of the file on the console
 print("Contents of 'my_file.txt':")
 print(file_contents)
+
+# Open the file "my_file.txt" in append mode ('a')
+with open("my_file.txt", "a") as file:
+    # Append three additional lines of text to the existing content
+    file.write(" remembered something, we need to append.\n")
+    file.write("Append this 0988676 and 67890\n")
+    file.write("Lets call it a function to open.\n")
+
+print("Three additional lines appended to 'my_file.txt'.")
+
